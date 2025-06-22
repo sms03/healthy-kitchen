e6c61d09a7181bd956edca60f7fc3c1ef80250c0
Shivam M. Salunkhe <84768081+sms03@users.noreply.github.com>
2025-06-22 13:16:35 +0530

    Update contact phone number in Contact component

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -71,7 +71,7 @@ const Contact = () => {
                       </div>
                       <div>
                         <h3 className="font-semibold text-gray-800">Phone</h3>
-                        <p className="text-gray-600">+91 98765 43210</p>
+                        <p className="text-gray-600">+91 9822042638</p>
                       </div>
                     </div>
                   </CardContent>
